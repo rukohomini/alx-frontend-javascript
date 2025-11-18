b93b8d4357aa5584c16b879ec10ea7f8aaeb3357
Rukoh Omini <rukohbenjamin@gmail.com>
2025-11-18 21:11:13 +0000

    Task 1 complete

---
+++ task_1/js/main.ts/main.ts
@@ -0,0 +1,20 @@
+// task_1/js/main.ts
+
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // allows additional properties
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
