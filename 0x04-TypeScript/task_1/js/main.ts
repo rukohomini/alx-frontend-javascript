736033b0b424b7e20e81c8e49a7bffe7c2dc3ea4
Rukoh Omini <rukohbenjamin@gmail.com>
2025-11-28 10:35:01 +0100

    Fix Task 1 and Task 2 interfaces for ALX checker

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,3 @@
-
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -7,17 +6,8 @@ interface Teacher {
   yearsOfExperience?: number;
   [key: string]: any;
 }
+
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-
-console.log(director1);
